83929993398b8f6b55d0246a65dae71d751276b5
ColtonMcCasland <36249316+ColtonMcCasland@users.noreply.github.com>
2022-04-18 10:12:48 -0500

    Update __init__.py

--- pypboy/__init__.py
+++ pypboy/__init__.py
@@ -91,11 +91,12 @@ class BaseModule(game.EntityGroup):
         if action.startswith("knob_"):
             if (Switch_A == 1) and (Switch_B == 0):
 
-                # if self.currentSubmodule >= self.submodules.__len__():
-                #     self.switch_submodule(0)
-                # else:
-                self.currentSubmodule += 1
-                self.switch_submodule(self.currentSubmodule)
+                if self.currentSubmodule >= self.submodules.__len__():
+                    # self.switch_submodule(0)
+                    return
+                else:
+                    self.currentSubmodule += 1
+                    self.switch_submodule(self.currentSubmodule)
 
                 print
                 "direction -> ", self.currentSubmodule
@@ -106,11 +107,12 @@ class BaseModule(game.EntityGroup):
                 return
 
             elif (Switch_A == 1) and (Switch_B == 1):
-                # if self.currentSubmodule < 0:
+                if self.currentSubmodule < 0:
+                    return
                     # self.currentSubmodule = self.submodules.__len__() - 1
-                # else:
-                self.currentSubmodule -= 1
-                self.switch_submodule(self.currentSubmodule)
+                else:
+                    self.currentSubmodule -= 1
+                    self.switch_submodule(self.currentSubmodule)
 
                 print
                 "direction <- ", self.currentSubmodule
