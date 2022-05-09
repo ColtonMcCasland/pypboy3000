896e812ca41c2b8d49b7e75eecefa79db2ee6101
ColtonMcCasland <36249316+ColtonMcCasland@users.noreply.github.com>
2022-05-09 17:52:07 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,12 +6,15 @@ import os, sys
 import config
 import platform
 
-parser = optparse.OptionParser(usage='python %prog -c True\nor:\npython %prog -c True', version="0.0.1", prog=sys.argv[0])
-parser.add_option('-c','--cached-map',        action="store_true", help="Loads the cached map file stored in map.cache", dest="load_cached", default=False)
+parser = optparse.OptionParser(usage='python %prog -c True\nor:\npython %prog -c True', version="0.0.1",
+                               prog=sys.argv[0])
+parser.add_option('-c', '--cached-map', action="store_true", help="Loads the cached map file stored in map.cache",
+                  dest="load_cached", default=False)
 options, args = parser.parse_args()
 
 try:
     import RPi.GPIO as GPIO
+
     GPIO.setmode(GPIO.BCM)
     config.GPIO_AVAILABLE = True
 except Exception:
@@ -20,11 +23,11 @@ except Exception:
     config.GPIO_AVAILABLE = False
 
 if config.GPIO_AVAILABLE:
-# Init framebuffer/touchscreen environment variables
+    # Init framebuffer/touchscreen environment variables
     os.putenv('SDL_VIDEODRIVER', 'fbcon')
-    os.putenv('SDL_FBDEV'      , '/dev/fb1')
-    os.putenv('SDL_MOUSEDRV'   , 'TSLIB')
-    os.putenv('SDL_MOUSEDEV'   , '/dev/input/touchscreen')
+    os.putenv('SDL_FBDEV', '/dev/fb1')
+    os.putenv('SDL_MOUSEDRV', 'TSLIB')
+    os.putenv('SDL_MOUSEDEV', '/dev/input/touchscreen')
     config.touchScale = 100
     config.invertPosition = True
 
@@ -32,9 +35,9 @@ from pypboy.core import Pypboy
 
 try:
     pygame.mixer.init(44100, -16, 2, 2048)
-    # config.SOUND_ENABLED = True
-except Exception as e:
-    print('Error on Sound!!!')
+    config.SOUND_ENABLED = True
+except:
+    config.SOUND_ENABLED = False
 
 if __name__ == "__main__":
     boy = Pypboy('Pip-Boy 3000', config.WIDTH, config.HEIGHT)
