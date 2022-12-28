2243f3d0b33b9af1ac54c7377b867e609034a138
ColtonMcCasland <36249316+ColtonMcCasland@users.noreply.github.com>
2022-12-28 15:47:31 -0600

    .

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ if config.GPIO_AVAILABLE:
     # os.putenv('SDL_VIDEODRIVER', 'fbcon')
     os.putenv('SDL_FBDEV', '/dev/fb1')
     os.putenv('SDL_MOUSEDRV', 'TSLIB')
-    os.putenv('SDL_MOUSEDEV', '/dev/input/touchscreen')
+    os.putenv('SDL_MOUSEDEV', '/dev/input/event0')
 
     
     config.touchScale = 100
