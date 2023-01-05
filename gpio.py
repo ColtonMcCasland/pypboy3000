53d3ba81a9a9cb2884d786c92c403071d4812996
ColtonMcCasland <36249316+ColtonMcCasland@users.noreply.github.com>
2023-01-05 14:19:51 -0600

    .

--- gpio.py
+++ gpio.py
@@ -1,14 +1,14 @@
 from gpiozero import Button
 from time import sleep
 
-button4 = Button(4)# used Stats
-button17 = Button(17) #used Items
-button18 = Button(18)
-button27 = Button(27) #used MAP
-button22 = Button(22)
+# button4 = Button(4)# used Stats
+# button17 = Button(17) #used Items
+# button18 = Button(18)
+# button27 = Button(27) #used MAP
+# button22 = Button(22)
 #button23 = Button(23) Reboots system if called pin 16
 #button24 = Button(24) No edge detection pin 18
-button25 = Button(25)
+# button25 = Button(25)
 while True:
     
     if button4.is_pressed:
@@ -19,14 +19,14 @@ while True:
         print("18 Pressed pin 12")
     elif button27.is_pressed:
         print("27 Pressed pin 13")
-    elif button22.is_pressed:
-        print("22 Pressed pin 15")
+    # elif button22.is_pressed:
+    #     print("22 Pressed pin 15")
     #elif button23.is_pressed:
     #    print("23 Pressed pin 16")
     #elif button24.is_pressed:
     #    print("24 Pressed pin 18")
-    elif button25.is_pressed:
-        print("25 Pressed pin 22")
+    # elif button25.is_pressed:
+    #     print("25 Pressed pin 22")
     else:
         print("Released")
     sleep(1)
