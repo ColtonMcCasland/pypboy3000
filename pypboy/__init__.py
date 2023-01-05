a43bd1bc8ebca6c64bfcbf9f2b25a6b9c5e53683
ColtonMcCasland <36249316+ColtonMcCasland@users.noreply.github.com>
2023-01-05 13:36:02 -0600

    .

--- pypboy/__init__.py
+++ pypboy/__init__.py
@@ -63,9 +63,11 @@ class BaseModule(game.EntityGroup):
             self.add(self.active)
             if config.GPIO_AVAILABLE:
                 print("led number ->  %d" % self.GPIO_LED_ID)
-                GPIO.setup(self.GPIO_LED_ID, GPIO.OUT)
-                GPIO.output(self.GPIO_LED_ID, True)
                 for i in gpioList:
+                    GPIO.setup(i, GPIO.OUT)
+                    GPIO.output(i, False)
+                    GPIO.setup(self.GPIO_LED_ID, GPIO.OUT)
+                    GPIO.output(self.GPIO_LED_ID, True)
                     print(i)
                     if self.GPIO_LED_ID != i:
                         GPIO.setup(i, GPIO.OUT)
