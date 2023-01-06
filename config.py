4eb03d00cc72f2a2c2b7fbcad830b870119ba061
ColtonMcCasland <36249316+ColtonMcCasland@users.noreply.github.com>
2023-01-06 13:48:21 -0600

    .

--- config.py
+++ config.py
@@ -64,10 +64,10 @@ ACTIONS = {
 #GPIO 23 pin16 reboot
 #GPIO 25 pin 22 blank screen do not use
 GPIO_ACTIONS = {
-	24: "knob_up", #GPIO 23
-	23: "knob_down", #GPIO 24
-	4: "dial_down", #GPIO 4
-	17: "dial_up", #GPIO 17
+	# 24: "knob_up", #GPIO 23
+	# 23: "knob_down", #GPIO 24
+	# 4: "dial_down", #GPIO 4
+	# 17: "dial_up", #GPIO 17
 }
 
 MAP_ICONS = {
