d42460bdae5f847bd0654ef0c92ec8387483f048
ColtonMcCasland <36249316+ColtonMcCasland@users.noreply.github.com>
2023-01-06 13:22:15 -0600

    .

--- gpio.py
+++ gpio.py
@@ -8,7 +8,7 @@ button27 = Button(27) #used MAP
 button22 = Button(22)
 #button23 = Button(23) Reboots system if called pin 16
 #button24 = Button(24) No edge detection pin 18
-button25 = Button(25)
+# button25 = Button(25)
 while True:
     
     if button4.is_pressed:
@@ -17,14 +17,14 @@ while True:
         print("17 Pressed pin 11")
     elif button18.is_pressed:
         print("18 Pressed pin 12")
-    elif button27.is_pressed:
-        print("27 Pressed pin 13")
+    # elif button27.is_pressed:
+    #     print("27 Pressed pin 13")
     elif button22.is_pressed:
         print("22 Pressed pin 15")
-    #elif button23.is_pressed:
-    #    print("23 Pressed pin 16")
-    #elif button24.is_pressed:
-    #    print("24 Pressed pin 18")
+    elif button23.is_pressed:
+       print("23 Pressed pin 16")
+    elif button24.is_pressed:
+       print("24 Pressed pin 18")
     elif button25.is_pressed:
         print("25 Pressed pin 22")
     else:
