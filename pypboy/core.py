05c51f4d567916924e5b801a6bc298072166669e
ColtonMcCasland <36249316+ColtonMcCasland@users.noreply.github.com>
2023-01-08 11:53:02 -0600

    .

--- pypboy/core.py
+++ pypboy/core.py
@@ -127,7 +127,7 @@ class Pypboy(game.core.Engine):
         #     self.mouseDownTime = pygame.time.get_ticks()
         #     self.mouseDownPos = pygame.mouse.get_pos()
         #     pygame.mouse.get_rel()
-        elif event.type == pygame.MOUSEBUTTONUP:
+        elif event.type == pygame.MOUSEBUTTONDOWN:
             # self.mouseUpPos = pygame.mouse.get_pos()
             # swipe = self.getSwipeType2()
             # swipe = self.getSwipeType()
