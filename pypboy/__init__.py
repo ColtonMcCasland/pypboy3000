c65ced6fbfc91caa498524e833042c2fd05ca414
ColtonMcCasland <36249316+ColtonMcCasland@users.noreply.github.com>
2023-01-09 11:41:35 -0600

    .

--- pypboy/__init__.py
+++ pypboy/__init__.py
@@ -22,6 +22,8 @@ class BaseModule(game.EntityGroup):
 
     def __init__(self, boy, *args, **kwargs):
         super(BaseModule, self).__init__()
+        
+        PIO.setup(self.GPIO_LED_ID, GPIO.OUT)
 
 
         self.pypboy = boy
@@ -109,6 +111,7 @@ class BaseModule(game.EntityGroup):
         self.currentSubmodule = 0
         self.switch_submodule(0)
         if config.GPIO_AVAILABLE:
+            GPIO.
             GPIO.output(self.GPIO_LED_ID, True)
         if config.SOUND_ENABLED:
             self.module_change_sfx.play()
